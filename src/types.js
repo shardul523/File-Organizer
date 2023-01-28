d116fe3e44dd231f59351dc33c35a4c93552d841
Shardul Sisodiya <98705625+shardul523@users.noreply.github.com>
2023-01-29 00:39:03 +0530

    Create types.js

---
+++ src/types.js
@@ -0,0 +1,11 @@
+types = {
+    'audio': ['mp3'],
+    'video': ['mp4', 'mkv', 'avi'],
+    'documents': ['docs', 'docx', 'pdf', 'txt', 'md', 'log'],
+    app: ['exe', 'msi'],
+    archives: ['rar', 'zip', '7z'],
+    images: ['jpeg', 'jpg', 'png', 'svg']
+};
+
+
+module.exports = types;
