df689c8a96a6043e6be73699c53eab452e162588
Shardul Sisodiya <98705625+shardul523@users.noreply.github.com>
2023-01-29 00:40:33 +0530

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,10 @@
+const helpFunc = () => {
+    console.log(`
+    The following commands can be performed:
+        1. node main.js tree 'directory path'
+        2. node main.js organize 'directory path'
+        3. node main.js help
+    `);
+};
+
+module.exports = helpFunc;
